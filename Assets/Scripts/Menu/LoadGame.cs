5fdeb0adae9ef29530603dd09adf7d5674ce5c7a
PhucNguyen08 <npnguyen812@gmail.com>
2023-05-05 15:50:06 +0700

    menu

---
+++ Assets/Scripts/Menu/LoadGame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public static string nameInput;
+    // Start is called before the first frame update
+    public void loadScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ReadStringInput()
+    {
+        nameInput = inputField.text;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player Has Quit The Game");
+    }
+}
